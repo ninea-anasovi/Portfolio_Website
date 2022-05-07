8d69aeff9f19534409410e6361a1984e381a65d0
Ninea Anasovi <ninea.anasovi@gmail.com>
2022-05-07 21:23:01 +0400

    Rebase ode

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss'
+import Sidebar from '../Sidebar'
+
+
+const Layout = () => {
+    return (
+    <>
+    <Sidebar/>
+    <h1>lalala</h1>
+    </>);
+}
+
+export  default Layout
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss';
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtite from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+
+ const Sidebar = () => {
+  return (
+    <div className='nav-bar'>
+
+        <Link to='/' className='logo'>
+            <img src={LogoS} alt ='logo'/>
+            <img className='sub-logo' src={LogoSubtite} alt ='logo'/>
+        </Link>
+
+      <nav>
+        <NavLink exact={true} to='/' className='active'>
+          <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+        </NavLink>
+        <NavLink exact={true} to='/about' className='active about-link'>
+          <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+        </NavLink>
+        <NavLink exact={true} to='/contact' className='active contact-link'>
+          <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+        </NavLink>        
+      </nav>
+    </div>
+  )
+}
+
+export default Sidebar
