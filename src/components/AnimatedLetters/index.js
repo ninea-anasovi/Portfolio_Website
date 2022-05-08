044925f55e0d5483eb2f2816dfeb918da6d16d71
Ninea Anasovi <ninea.anasovi@gmail.com>
2022-05-09 02:03:58 +0400

    started working on front animations

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,18 @@
+import './index.scss'
+import React from 'react'
+
+function AnimatedLetters({letterClass, strArray, index}) {
+  return (
+    <span>
+        {
+            strArray.map( (char,i)=>(
+                <span key={char+i} className={`${letterClass} _${i}`}>
+                    {char}
+                </span>
+            ))
+        }
+    </span>
+  )
+}
+
+export default AnimatedLetters
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,41 @@
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['I',"'",'m',' ','N','i','n','e','a'];
+    const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+   
+  useEffect ( () => {
+      return setTimeout(() => {
+      setLetterClass('text-animate-hover')},4000)
+    }, [])
+
     return (
 
         <div className='container home-page'>
             <div className='text-zone'>
-                <h1>Hi, <br/> I'm
-                Ninea <br/> Web Developer</h1>
+                <h1>Hi, <br/> <AnimatedLetters className={letterClass} strArray={nameArray} index={15}/> <br/> Web Developer</h1>
                 <h2>React Developer | Programmer </h2>
-                <Link to='/contact'> CONTACT ME</Link>
+                <Link to='/contact' className='flat-button'> CONTACT ME</Link>
 
             </div>
         </div>
