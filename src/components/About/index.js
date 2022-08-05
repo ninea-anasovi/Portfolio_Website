0fb13f56dfd4c1c8080ca9d83113aebab6e610b0
Ninea Anasovi <ninea.anasovi@gmail.com>
2022-08-05 15:52:07 +0400

    text changed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -25,16 +25,13 @@ export default function About() {
         <h1>
           <AnimatedLetters inputText={'About Me'} time={5} letterClass={letterClass}/>
         </h1>
+
         <p>
-         As a chilt I was absolutely passionate about 2 things: playing with Visuals (Drawing, Photographing, Digital Art) and Technical Problem Solving (Math, physics) as I grew up I decided that my profession should be Connected to my technical side and settled to Study Electrical and Computer Engineering but never stopped developing my art side. 
+          I graduated from Agricultural University of Georgia in a department of Electrical and Computer Engineering, that gave me solid knowledge of Hardware technologies from PCB designing to Hardware (programming microcontrollers and controllers) and Software Programming.
         </p>
 
         <p>
-          Electrical and Computer Engineering in Agricultural University of Georgia gave me solid knowledge of Hardware technologies from PCB designing to Hardware (programming microcontrollers and controllers) and Software Programming.
-        </p>
-
-        <p>
-          Alongside I learned Graphic Design to not give up my drawing skills and enhance, than I realized that what could bring both Together and let me to fully develop both of my sides would be Frontend Development and started learning that with solid knowledge of other programming languages like - C++, Python and little bit of Assembler, which helped me a lot. And my learning Journey was more of a fun opportunity to draw with a code.
+          Alongside I learned Graphic Design to not give up my drawing skills, than I realized that what could bring both Together and let me to fully develop both of my sides would be Frontend Development and started learning that with solid knowledge of other programming languages like - C++, Python and little bit of Assembler, which helped me a lot. And my learning Journey was more of a fun opportunity to draw with a code.
         </p>
 
         <p>
