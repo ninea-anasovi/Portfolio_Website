3378d53ee7ddc82b9e7e917f7a49f4c4f0c23586
Ninea Anasovi <ninea.anasovi@gmail.com>
2022-08-11 00:30:21 +0400

    creating cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,6 +4,88 @@ import './index.scss'
 function Card() {
   return (
     <>
+    <ul class="container cards-portfolio">
+      <li>
+        <a href="" class="card-portfolio">
+          <img src="https://i.imgur.com/oYiTqum.jpg" class="card-image" alt="" />
+          <div class="card-overlay">
+            <div class="card-header">
+              <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+              <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+              <div class="card-header-text">
+                <h3 class="card__title">Jessica Parker</h3>            
+                <span class="card__status">1 hour ago</span>
+              </div>
+            </div>
+            <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+          </div>
+        </a>      
+      </li>
+      <li>
+        <a href="" class="card-portfolio">
+          <img src="https://i.imgur.com/oYiTqum.jpg" class="card-image" alt="" />
+          <div class="card-overlay">
+            <div class="card-header">
+              <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+              <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+              <div class="card-header-text">
+                <h3 class="card__title">Jessica Parker</h3>            
+                <span class="card__status">1 hour ago</span>
+              </div>
+            </div>
+            <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+          </div>
+        </a>      
+      </li>
+      <li>
+        <a href="" class="card-portfolio">
+          <img src="https://i.imgur.com/oYiTqum.jpg" class="card-image" alt="" />
+          <div class="card-overlay">
+            <div class="card-header">
+              <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+              <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+              <div class="card-header-text">
+                <h3 class="card__title">Jessica Parker</h3>            
+                <span class="card__status">1 hour ago</span>
+              </div>
+            </div>
+            <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+          </div>
+        </a>      
+      </li>
+      <li>
+        <a href="" class="card-portfolio">
+          <img src="https://i.imgur.com/oYiTqum.jpg" class="card-image" alt="" />
+          <div class="card-overlay">
+            <div class="card-header">
+              <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+              <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+              <div class="card-header-text">
+                <h3 class="card__title">Jessica Parker</h3>            
+                <span class="card__status">1 hour ago</span>
+              </div>
+            </div>
+            <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+          </div>
+        </a>      
+      </li>
+      <li>
+        <a href="" class="card-portfolio">
+          <img src="https://i.imgur.com/oYiTqum.jpg" class="card-image" alt="" />
+          <div class="card-overlay">
+            <div class="card-header">
+              <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+              <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+              <div class="card-header-text">
+                <h3 class="card__title">Jessica Parker</h3>            
+                <span class="card__status">1 hour ago</span>
+              </div>
+            </div>
+            <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+          </div>
+        </a>      
+      </li>
+    </ul>
     </>
   )
 }
